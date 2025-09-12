97a631ef1fbe58afb2a20d74b40c49d2c08d9bb6
CYA <cya@CYA.local>
2025-09-12 19:21:20 +0800

    Format .nix files

--- hosts/nixos-vm/hardware-configuration.nix
+++ hosts/nixos-vm/hardware-configuration.nix
@@ -1,26 +1,42 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "ohci_pci"
+    "ehci_pci"
+    "ahci"
+    "sd_mod"
+    "sr_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1d978d65-2bf8-4275-bd88-248cf3984985";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/1d978d65-2bf8-4275-bd88-248cf3984985";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EAA9-539B";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/40ae21aa-3842-4e41-b262-682124bdf3ae"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/EAA9-539B";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/40ae21aa-3842-4e41-b262-682124bdf3ae"; }
+  ];
 
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
