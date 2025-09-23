447579205e531456fc49bab431f2df5c2bd38292
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-09-23 20:31:52 +0800

    Update zsh default.nix

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -4,6 +4,16 @@
     zsh = {
       enable = true;
       enableCompletion = true;
+      autosuggestion = {
+        enable = true;
+      };
+      syntaxHighlighting = {
+        enable = true;
+      };
+      autosuggestion = {
+        highlight = "fg=#ff00ff,bg=cyan,bold,underline";
+        strategy = [ "history" "completion" "match_prev_cmd"];
+      };
       shellAliases = {
         ls = "lsd";
         ff = "fastfetch";
