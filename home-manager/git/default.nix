2cc535ef4eb29f07b2a88bdc5f316369555c3cf3
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-10 13:17:06 +0800

    Update git config

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -9,6 +9,28 @@
       enable = true;
       userName = "MushMushroom0809";
       userEmail = "cya.chen.ya@gmail.com";
+      delta = {
+        enable = true;
+        options = {
+          decorations = {
+            commit-decoration-style = "bold yellow box ul";
+            file-decoration-style = "none";
+            file-style = "bold yellow ul";
+          };
+          features = "decorations";
+          whitespace-error-style = "22 reverse";
+        };
+      };
+      extraConfig = {
+        core = {
+          whitespace = "trailing-space,space-before-tab";
+        };
+      };
+      ignores = [
+        "*.elc"
+        "*.eln"
+        "*.swp"
+      ];
     };
   };
 }
