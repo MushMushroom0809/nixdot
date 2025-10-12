0f38846d81e3175c03a1171b42518a0a97ad0b26
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-12 21:28:00 +0800

    Change helix config

--- home-manager/helix/default.nix
+++ home-manager/helix/default.nix
@@ -7,6 +7,45 @@
   programs = {
     helix = {
       enable = true;
+      defaultEditor = false;
+      settings = {
+        theme = "github_dark_high_contrast";
+        editor = {
+          bufferline = "always";
+          text-width = 80;
+          rulers = [ 80 ];
+          line-number = "relative";
+          auto-save = true;
+          statusline = {
+            left = [
+              "mode"
+              "spacer"
+              "version-control"
+              "spinner"
+              "read-only-indicator"
+              "file-modification-indicator"
+            ];
+            mode = {
+              normal = "NORMAL";
+              insert = "INSERT";
+              select = "SELECT";
+            };
+          };
+          indent-guides = {
+            render = true;
+          };
+          cursor-shape = {
+            insert = "bar";
+          };
+        };
+        keys = {
+          insert = {
+            "j" = {
+              "k" = "normal_mode";
+            };
+          };
+        };
+      };
     };
   };
 }
