3a53d69074487aff81a6e3679b9af3860b519ca1
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-12 09:59:41 +0800

    Add more font

--- home-manager/fonts/default.nix
+++ home-manager/fonts/default.nix
@@ -3,13 +3,15 @@
   ...
 }:
 {
-  home.packages = with pkgs.nerd-fonts; [
-    hack
-    jetbrains-mono
-    noto
-    ubuntu
-    ubuntu-mono
-    victor-mono
+  home.packages = with pkgs; [
+    nerd-fonts.hack
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.noto
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.victor-mono
+    wqy_zenhei
+    wqy_microhei
   ];
   fonts = {
     fontconfig = {
