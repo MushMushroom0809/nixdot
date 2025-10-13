74f95cb6c1f0d0fa6188026223e311fbf4f09f54
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-13 08:47:53 +0800

    Update default.nix

--- home-manager/vscode/default.nix
+++ home-manager/vscode/default.nix
@@ -4,116 +4,118 @@
   ...
 }:
 {
-  programs.vscode = {
-    enable = true;
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          aaron-bond.better-comments
-          arrterian.nix-env-selector
-          bbenoist.nix
-          formulahendry.code-runner
-          github.github-vscode-theme
-          gruntfuggly.todo-tree
-          jgclark.vscode-todo-highlight
-          jnoortheen.nix-ide
-          jeff-hykin.better-nix-syntax
-          llvm-vs-code-extensions.vscode-clangd
-          mechatroner.rainbow-csv
-          ms-python.black-formatter
-          ms-python.debugpy
-          ms-python.flake8
-          ms-python.isort
-          ms-python.python
-          ms-python.pylint
-          ms-python.mypy-type-checker
-          ms-python.vscode-pylance
-          ms-toolsai.jupyter
-          ms-toolsai.jupyter-renderers
-          ms-toolsai.jupyter-keymap
-          ms-toolsai.vscode-jupyter-slideshow
-          ms-toolsai.vscode-jupyter-cell-tags
-          ms-vscode.remote-explorer
-          ms-vscode.live-server
-          redhat.vscode-yaml
-          redhat.vscode-xml
-          sumneko.lua
-          tamasfe.even-better-toml
-          vadimcn.vscode-lldb
-          vincaslt.highlight-matching-tag
-          vscodevim.vim
-          vscode-icons-team.vscode-icons
-          yzhang.markdown-all-in-one
-          zainchen.json
-        ];
-        userSettings = {
-          "editor.fontSize" = 15;
-          "editor.fontFamily" = "JetBrainsMono Nerd Font";
-          "editor.fontLigatures" = true;
-          "editor.rulers" = [ 80 ];
-          "editor.guides.bracketPairs" = true;
-          "extensions.experimental.affinity" = {
-            "vscodevim.vim" = 1;
+  programs = {
+    vscode = {
+      enable = true;
+      profiles = {
+        default = {
+          extensions = with pkgs.vscode-extensions; [
+            aaron-bond.better-comments
+            arrterian.nix-env-selector
+            bbenoist.nix
+            formulahendry.code-runner
+            github.github-vscode-theme
+            gruntfuggly.todo-tree
+            jgclark.vscode-todo-highlight
+            jnoortheen.nix-ide
+            jeff-hykin.better-nix-syntax
+            llvm-vs-code-extensions.vscode-clangd
+            mechatroner.rainbow-csv
+            ms-python.black-formatter
+            ms-python.debugpy
+            ms-python.flake8
+            ms-python.isort
+            ms-python.python
+            ms-python.pylint
+            ms-python.mypy-type-checker
+            ms-python.vscode-pylance
+            ms-toolsai.jupyter
+            ms-toolsai.jupyter-renderers
+            ms-toolsai.jupyter-keymap
+            ms-toolsai.vscode-jupyter-slideshow
+            ms-toolsai.vscode-jupyter-cell-tags
+            ms-vscode.remote-explorer
+            ms-vscode.live-server
+            redhat.vscode-yaml
+            redhat.vscode-xml
+            sumneko.lua
+            tamasfe.even-better-toml
+            vadimcn.vscode-lldb
+            vincaslt.highlight-matching-tag
+            vscodevim.vim
+            vscode-icons-team.vscode-icons
+            yzhang.markdown-all-in-one
+            zainchen.json
+          ];
+          userSettings = {
+            "editor.fontSize" = 15;
+            "editor.fontFamily" = "JetBrainsMono Nerd Font";
+            "editor.fontLigatures" = true;
+            "editor.rulers" = [ 80 ];
+            "editor.guides.bracketPairs" = true;
+            "extensions.experimental.affinity" = {
+              "vscodevim.vim" = 1;
+            };
+            "window.zoomLevel" = -0.75;
+            "workbench.colorTheme" = "GitHub Dark Default";
+            "workbench.iconTheme" = "vscode-icons";
+            "vim.highlightedyank.enable" = true;
+            "vim.hlsearch" = true;
+            "vim.insertModeKeyBindingsNonRecursive" = [
+              {
+                "before" = [
+                  "j"
+                  "k"
+                ];
+                "after" = [
+                  "<Esc>"
+                ];
+              }
+            ];
+            "vim.normalModeKeyBindingsNonRecursive" = [
+              {
+                "before" = [
+                  "K"
+                ];
+                "commands" = [
+                  "editor.action.showHover"
+                ];
+              }
+              {
+                "before" = [
+                  "g"
+                  "d"
+                ];
+                "commands" = [
+                  "editor.action.revealDefinition"
+                ];
+              }
+            ];
+            "vim.useSystemClipboard" = true;
           };
-          "window.zoomLevel" = -0.75;
-          "workbench.colorTheme" = "GitHub Dark Default";
-          "workbench.iconTheme" = "vscode-icons";
-          "vim.highlightedyank.enable" = true;
-          "vim.hlsearch" = true;
-          "vim.insertModeKeyBindingsNonRecursive" = [
+          keybindings = [
             {
-              "before" = [
-                "j"
-                "k"
-              ];
-              "after" = [
-                "<Esc>"
-              ];
+              "key" = "ctrl+h";
+              "command" = "workbench.action.navigateLeft";
+              "when" = "editorTextFocus";
+            }
+            {
+              "key" = "ctrl+l";
+              "command" = "workbench.action.navigateRight";
+              "when" = "editorTextFocus";
+            }
+            {
+              "key" = "ctrl+k";
+              "command" = "workbench.action.navigateUp";
+              "when" = "editorTextFocus";
+            }
+            {
+              "key" = "ctrl+j";
+              "command" = "workbench.action.navigateDown";
+              "when" = "editorTextFocus";
             }
           ];
-          "vim.normalModeKeyBindingsNonRecursive" = [
-            {
-              "before" = [
-                "K"
-              ];
-              "commands" = [
-                "editor.action.showHover"
-              ];
-            }
-            {
-              "before" = [
-                "g"
-                "d"
-              ];
-              "commands" = [
-                "editor.action.revealDefinition"
-              ];
-            }
-          ];
-          "vim.useSystemClipboard" = true;
         };
-        keybindings = [
-          {
-            "key" = "ctrl+h";
-            "command" = "workbench.action.navigateLeft";
-            "when" = "editorTextFocus";
-          }
-          {
-            "key" = "ctrl+l";
-            "command" = "workbench.action.navigateRight";
-            "when" = "editorTextFocus";
-          }
-          {
-            "key" = "ctrl+k";
-            "command" = "workbench.action.navigateUp";
-            "when" = "editorTextFocus";
-          }
-          {
-            "key" = "ctrl+j";
-            "command" = "workbench.action.navigateDown";
-            "when" = "editorTextFocus";
-          }
-        ];
       };
     };
   };
