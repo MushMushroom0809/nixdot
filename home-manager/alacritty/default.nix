09cd33c69608acd2bd9ca8de8479feb55d09d7b0
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-13 08:50:39 +0800

    Update default.nix

--- home-manager/alacritty/default.nix
+++ home-manager/alacritty/default.nix
@@ -4,54 +4,56 @@
   ...
 }:
 {
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = {
-        padding = {
-          x = 12;
-          y = 12;
+  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          padding = {
+            x = 8;
+            y = 8;
+          };
+          dynamic_padding = true;
+          decorations = "Full";
+          opacity = 0.95;
+          blur = true;
+          startup_mode = "Maximized";
+          decorations_theme_variant = "Dark";
+          resize_increments = true;
+          option_as_alt = "OnlyLeft";
         };
-        dynamic_padding = true;
-        decorations = "Full";
-        opacity = 0.98;
-        blur = true;
-        startup_mode = "Maximized";
-        decorations_theme_variant = "Dark";
-        resize_increments = true;
-        option_as_alt = "OnlyLeft";
-      };
-      scrolling = {
-        history = 10000;
-        multiplier = 5;
-      };
-      font = {
-        normal = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Regular";
+        scrolling = {
+          history = 10000;
+          multiplier = 5;
         };
-        bold = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Bold";
+        font = {
+          normal = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Bold";
+          };
+          size = 12.0;
         };
-        size = 12.0;
-      };
-      bell = {
-        animation = "EaseOut";
-        duration = 3;
-      };
-      cursor = {
-        style = {
-          shape = "Block";
-          blinking = "Always";
+        bell = {
+          animation = "EaseOut";
+          duration = 3;
         };
-        blink_interval = 1000;
-        thickness = 0.2;
+        cursor = {
+          style = {
+            shape = "Block";
+            blinking = "Always";
+          };
+          blink_interval = 1000;
+          thickness = 0.2;
+        };
+        mouse = {
+          hide_when_typing = true;
+        };
       };
-      mouse = {
-        hide_when_typing = true;
-      };
+      theme = "github_dark_high_contrast";
     };
-    theme = "github_dark_high_contrast";
   };
 }
