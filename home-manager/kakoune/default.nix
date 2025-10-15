cea0f53194e42fef6dc6953e9189bf99d20ce39a
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-15 20:55:52 +0800

    Update kak config

--- home-manager/kakoune/default.nix
+++ home-manager/kakoune/default.nix
@@ -7,10 +7,42 @@
   programs = {
     kakoune = {
       enable = true;
-      colorSchemePackage = with pkgs; [
-        kakounePlugins.kakoune-catppuccin
+      colorSchemePackage = pkgs.kakounePlugins.kakoune-catppuccin;
+      plugins = with pkgs; [
+        kakounePlugins.kak-fzf
       ];
-      config = {};
+      config = {
+        autoComplete = [
+          "insert"
+          "prompt"
+        ];
+        autoInfo = [
+          "command"
+          "onkey"
+        ];
+        autoReload = "yes";
+        colorScheme = "catppuccin_mocha";
+        indentWidth = 2;
+        numberLines = {
+          enable = true;
+          highlightCursor = true;
+          relative = true;
+        };
+        scrollOff = {
+          columns = 2;
+          lines = 2;
+        };
+        showMatching = true;
+        showWhitespace = {
+          enable = true;
+        };
+        ui = {
+          enableMouse = true;
+          assistant = "cat";
+          changeColors = true;
+          statusLine = "bottom";
+        };
+      };
     };
   };
 }
