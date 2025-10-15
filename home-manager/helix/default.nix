f72eb668add5275ce935e1a7124afa94508a2fd1
MushMushroom0809 <cya.chen.ya@gmail.com>
2025-10-15 19:51:57 +0800

    Update helix config

--- home-manager/helix/default.nix
+++ home-manager/helix/default.nix
@@ -16,6 +16,8 @@
           rulers = [ 80 ];
           line-number = "relative";
           auto-save = true;
+          true-color = true;
+          color-modes = true;
           statusline = {
             left = [
               "mode"
@@ -24,6 +26,17 @@
               "spinner"
               "read-only-indicator"
               "file-modification-indicator"
+            ];
+            center = [
+              "workspace-diagnostics"
+            ];
+            right = [
+              "file-type"
+              "position"
+              "total-line-numbers"
+              "position-percentage"
+              "file-encoding"
+              "file-line-ending"
             ];
             mode = {
               normal = "NORMAL";
@@ -34,15 +47,84 @@
           indent-guides = {
             render = true;
           };
+          inline-diagnostics = {
+            cursor-line = "warning";
+          };
           cursor-shape = {
             insert = "bar";
           };
         };
         keys = {
+          normal = {
+            "v" = [
+              "collapse_selection"
+              "select_mode"
+            ];
+            "V" = [
+              "select_mode"
+              "extend_to_line_bounds"
+            ];
+            "0" = "goto_line_start";
+            "$" = "goto_line_end";
+            "%" = "match_brackets";
+            "G" = "goto_file_end";
+            "x" = "delete_selection";
+            "p" = [
+              "paste_clipboard_after"
+              "collapse_selection"
+            ];
+            "P" = [
+              "paste_clipboard_before"
+              "collapse_selection"
+            ];
+            "esc" = [
+              "collapse_selection"
+              "keep_primary_selection"
+            ];
+            "d" = {
+              "d" = [
+                "extend_to_line_bounds"
+                "yank_main_selection_to_clipboard"
+                "delete_selection"
+              ];
+            };
+            "y" = {
+              "y" = [
+                "extend_to_line_bounds"
+                "yank_main_selection_to_clipboard"
+                "normal_mode"
+                "collapse_selection"
+              ];
+            };
+          };
           insert = {
             "j" = {
               "k" = "normal_mode";
             };
+            "esc" = [
+              "collapse_selection"
+              "normal_mode"
+            ];
+          };
+          select = {
+            "0" = "goto_line_start";
+            "$" = "goto_line_end";
+            "G" = "goto_file_end";
+            "x" = [
+              "yank_main_selection_to_clipboard"
+              "delete_selection"
+            ];
+            "y" = [
+              "yank_main_selection_to_clipboard"
+              "normal_mode"
+              "flip_selections"
+              "collapse_selection"
+            ];
+            "esc" = [
+              "collapse_selection"
+              "keep_primary_selection"
+              "normal_mode"
+            ];
           };
         };
       };
